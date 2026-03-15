c900728bb46396c40f5411d863249efe1941983c
SriGanesh01 <psriganesh2005@gmail.com>
2026-03-15 23:34:06 +0530

    Cyllwnder Generation. Look Into Again

--- SourceFiles/CylinderMeshGenerator.cpp
+++ SourceFiles/CylinderMeshGenerator.cpp
@@ -5,6 +5,8 @@
 #define M_PI 3.14159265358979323846f
 #endif
 
+// I'm CRYINGGGG. HOW DO I DRAW A SHAPE MATHEMATICALLYYYYYYY
+
 GenerateCylinderFlatValues::GenerateCylinderFlatValues()
 {
     float radius = 1.0f;
@@ -15,6 +17,7 @@ GenerateCylinderFlatValues::GenerateCylinderFlatValues()
 
     float heightStep = height / heightSegments;
     float thetaStep = (2.0f * M_PI) / radialSegments;
+
 
     for (int i = 0; i <= heightSegments; i++)
     {
@@ -35,6 +38,7 @@ GenerateCylinderFlatValues::GenerateCylinderFlatValues()
 
     int ringVertexCount = radialSegments + 1;
 
+
     for (int i = 0; i < heightSegments; i++)
     {
         for (int j = 0; j < radialSegments; j++)
@@ -50,6 +54,71 @@ GenerateCylinderFlatValues::GenerateCylinderFlatValues()
             cylinderIndices.push_back(next);
             cylinderIndices.push_back(next + 1);
         }
+    }
+
+
+    int bottomCenterIndex = cylinderVertices.size() / 3;
+
+    // center
+    cylinderVertices.push_back(0.0f);
+    cylinderVertices.push_back(-height / 2);
+    cylinderVertices.push_back(0.0f);
+
+    // ring
+    for (int j = 0; j < radialSegments; j++)
+    {
+        float theta = j * thetaStep;
+
+        float x = radius * cosf(theta);
+        float z = radius * sinf(theta);
+
+        cylinderVertices.push_back(x);
+        cylinderVertices.push_back(-height / 2);
+        cylinderVertices.push_back(z);
+    }
+
+    int start = bottomCenterIndex + 1;
+
+    for (int j = 0; j < radialSegments; j++)
+    {
+        int current = start + j;
+        int next = start + (j + 1) % radialSegments;
+
+        cylinderIndices.push_back(bottomCenterIndex);
+        cylinderIndices.push_back(current);
+        cylinderIndices.push_back(next);
+    }
+
+    int topCenterIndex = cylinderVertices.size() / 3;
+
+    // center
+    cylinderVertices.push_back(0.0f);
+    cylinderVertices.push_back(height / 2);
+    cylinderVertices.push_back(0.0f);
+
+    // ring
+    for (int j = 0; j < radialSegments; j++)
+    {
+        float theta = j * thetaStep;
+
+        float x = radius * cosf(theta);
+        float z = radius * sinf(theta);
+
+        cylinderVertices.push_back(x);
+        cylinderVertices.push_back(height / 2);
+        cylinderVertices.push_back(z);
+    }
+
+    start = topCenterIndex + 1;
+
+    for (int j = 0; j < radialSegments; j++)
+    {
+        int current = start + j;
+        int next = start + (j + 1) % radialSegments;
+
+        cylinderIndices.push_back(topCenterIndex);
+        cylinderIndices.push_back(next);
+        cylinderIndices.push_back(current);
     }
 }
 
