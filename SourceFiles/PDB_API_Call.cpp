b9f98993752f3c17706735a466380a7924c271e9
SriGanesh01 <psriganesh2005@gmail.com>
2026-03-22 23:59:44 +0530

    Stuff To Output into a FILE?

--- SourceFiles/PDB_API_Call.cpp
+++ SourceFiles/PDB_API_Call.cpp
@@ -1,21 +1,25 @@
 
 #include <curl/curl.h>
 #include <iostream>
+#include <fstream>
 
 #include "../HeaderFiles/PDB_API_Call.h"
 
-size_t WriteToPDBFile(char* buffer, size_t itemSize, size_t numberOfItems, void* ignoreForNow_UsedToPassInAdditionalObjects) // Is a must to have all these arguements; This is a callback function btw;
+size_t WriteToPDBFile(char* ActualContent, size_t itemSize, size_t numberOfItems, void* ignoreForNow_UsedToPassInAdditionalObjectsLikeFilesToWriteOutputIn) // Is a must to have all these arguements; This is a callback function btw;
 {
 	size_t bytes = itemSize * numberOfItems; // To verify that all data is processed correctly; Just have this thing;
 
 	std::cout << bytes << std::endl;
 
-	for (int i = 0; i < bytes; i++)
+	/*for (int i = 0; i < bytes; i++)
 	{
 		std::cout << buffer[i];
-	}
+	}*/
 
-	return bytes;
+	FILE* PDBfile = (FILE*)ignoreForNow_UsedToPassInAdditionalObjectsLikeFilesToWriteOutputIn;
+	
+
+	return fwrite(ActualContent, itemSize, numberOfItems, PDBfile);
 }
 
 
@@ -35,10 +39,12 @@ void CallPDBAPI(std::string PDBID)
 
 	if (curl != NULL)
 	{
-
+		FILE* PDBFile = fopen(SavingFileName.c_str(), "wb");
 		// Set Options
 		curl_easy_setopt(curl, CURLOPT_URL, PDB_Download_URL.c_str());
 		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteToPDBFile); // Rewrites Defauult output behaviour; Which is to stdout to the terminal;
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, PDBFile);
+
 
 		// Perform Actions
 		CURLcode result = curl_easy_perform(curl);
@@ -48,6 +54,7 @@ void CallPDBAPI(std::string PDBID)
 			std::cout << "File Not Found" << std::endl;
 		}
 
+		fclose(PDBFile);
 		curl_easy_cleanup(curl);
 	}
 }
